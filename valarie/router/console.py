4a511657dac1605d627c119f0820373b0ab335b9
Justin Dierking <phnomcobra@gmail.com>
2022-06-10 15:45:19 -0400

    doc strings and cleanup for console router.

--- valarie/router/console.py
+++ valarie/router/console.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
+"""This module implements the console routes."""
+
+import json
 
 import cherrypy
-import json
-import traceback
 
-from valarie.router.messaging import add_message
 from valarie.controller.console import get_consoles
 
-class Console(object):
+class Console(): # pylint: disable=too-few-public-methods
+    """This class encapsulates the console routes."""
+    @classmethod
     @cherrypy.expose
-    def get_consoles(self):
-        try:
-            return json.dumps(get_consoles())
-        except:
-            add_message(traceback.format_exc())
+    def get_consoles(cls) -> str:
+        """This function returns a list of console objects used in
+        the frontend for populating the console radio button control in host attributes.
+
+        Returns:
+            JSON string of consoles.
+        """
+        return json.dumps(get_consoles())
