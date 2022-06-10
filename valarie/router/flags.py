aeec654339fe9230f50e26d0ddc03ed57e60f2e0
Justin Dierking <phnomcobra@gmail.com>
2022-06-10 16:11:25 -0400

    flags router

--- valarie/router/flags.py
+++ valarie/router/flags.py
@@ -1,56 +1,69 @@
 #!/usr/bin/python3
+"""This module implements the flag routes used to
+signal the UI to take some action on a flag being changed."""
+
+import json
+from typing import Any
 
 import cherrypy
-import json
 
-from threading import Lock
-from random import random
-from copy import deepcopy
+from valarie.controller.flags import get_flag, set_flag, touch_flag
 
-flags = {}
-flag_lock = Lock()
-
-def set_flag(key, value):
-    flag_lock.acquire()
-    flags[key] = deepcopy(value)
-    flag_lock.release()
-    return value
-
-def get_flag(key):
-    try:
-        flag_lock.acquire()
-        value = deepcopy(flags[key])
-    except KeyError:
-        flags[key] = random()
-        value = deepcopy(flags[key])
-    finally:
-        flag_lock.release()
-        return value
-
-def touch_flag(key):
-    return set_flag(key, random())
-
-class Flags(object):
+class Flags():
+    """This class encapsulates the flag routes."""
+    @classmethod
     @cherrypy.expose
-    def set(self, key, value):
+    def set(cls, key: str, value: Any) -> str:
+        """This function sets the value of a flag and returns the key and value.
+
+        Args:
+            key:
+                Key name of the flag.
+
+            value:
+                Value to set the flag to.
+
+        Returns:
+            JSON string of the flag's key and value.
+        """
         item = {
-            "key" : key, 
+            "key" : key,
             "value" : set_flag(key, value)
         }
         return json.dumps(item)
-    
+
+    @classmethod
     @cherrypy.expose
-    def get(self, key):
+    def get(cls, key: str) -> str:
+        """This function gets the value of a flag and returns the key and value.
+
+        Args:
+            key:
+                Key name of the flag.
+
+        Returns:
+            JSON string of the flag's key and value.
+        """
         item = {
-            "key" : key, 
+            "key" : key,
             "value" : get_flag(key)
         }
         return json.dumps(item)
-    
+
+    @classmethod
     @cherrypy.expose
-    def touch(self, key):
+    def touch(cls, key: str) -> str:
+        """This function touches the value of a flag and returns the key and value.
+
+        Args:
+            key:
+                Key name of the flag.
+
+        Returns:
+            JSON string of the flag's key and value.
+        """
         item = {
-            "key" : key, 
+            "key" : key,
             "value" : touch_flag(key)
         }
         return json.dumps(item)
