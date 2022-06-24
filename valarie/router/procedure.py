a540f5967265c8dd595378f6b172fc8f2dcbf1eb
Justin Dierking <phnomcobra@gmail.com>
2022-06-24 09:59:51 -0400

    procedure router cleanup and doc strings.

--- valarie/router/procedure.py
+++ valarie/router/procedure.py
@@ -1,57 +1,101 @@
 #!/usr/bin/python3
+"""This module implements the routes for getting a procedure's task and
+host grids, queuing single and multiple procedures, and getting the queue
+grid."""
+
+from typing import Dict, List
 
 import cherrypy
-import json
-import traceback
 
-from valarie.controller.messaging import add_message
 from valarie.controller.procedure import get_task_grid, get_host_grid
 from valarie.executor.procedure import get_jobs_grid, queue_procedure
 
-class Procedure(object):
+class Procedure():
+    """This class registers methods as routes for getting a procedure's
+    task and host grids, queuing single and multiple procedures, and
+    getting the queue grid."""
+    @classmethod
     @cherrypy.expose
-    def get_task_grid(self, objuuid):
-        try:
-            return json.dumps(get_task_grid(objuuid))
-        except:
-            add_message(traceback.format_exc())
+    @cherrypy.tools.json_out()
+    def get_task_grid(cls, objuuid: str) -> List[Dict]:
+        """This function registers the endpoint that returns a procedure's
+        list of tasks.
 
+        Args:
+            objuuid:
+                A procedure's UUID.
+
+        Returns:
+            A list of task items for jsgrid.
+        """
+        return get_task_grid(objuuid)
+
+    @classmethod
     @cherrypy.expose
-    def get_host_grid(self, objuuid):
-        try:
-            return json.dumps(get_host_grid(objuuid))
-        except:
-            add_message(traceback.format_exc())
+    @cherrypy.tools.json_out()
+    def get_host_grid(cls, objuuid: str) -> List[Dict]:
+        """This function registers the endpoint that returns a procedure's
+        list of hosts.
 
+        Args:
+            objuuid:
+                A procedure's UUID.
+
+        Returns:
+            A list of host items for jsgrid.
+        """
+        return get_host_grid(objuuid)
+
+    @classmethod
     @cherrypy.expose
-    def queue_procedure(self, prcuuid, hstuuid):
-        try:
-            queue_procedure(hstuuid, prcuuid)
-            return json.dumps({})
-        except:
-            add_message(traceback.format_exc())
+    @cherrypy.tools.json_out()
+    def queue_procedure(cls, prcuuid: str, hstuuid: str) -> Dict:
+        """This function registers the endpoint that queues a procedure
+        for execution for a specified host.
 
+        Args:
+            prcuuid:
+                A procedure's UUID.
+
+            hstuuid:
+                A host's UUID.
+        """
+        queue_procedure(hstuuid, prcuuid)
+        return {}
+
+    @classmethod
     @cherrypy.expose
-    def queue_procedures(self, queuelist):
-        try:
-            for item in json.loads(queuelist):
-                try:
-                    if "ctruuid" in item:
-                        ctruuid = item["ctruuid"]
-                    else:
-                        ctruuid = None
+    @cherrypy.tools.json_in()
+    @cherrypy.tools.json_out()
+    def queue_procedures(cls, queuelist: List[Dict]) -> List[Dict]:
+        """This function registers the endpoint that queues multiple
+        procedures execution.
 
-                    queue_procedure(item["hstuuid"], item["prcuuid"], ctruuid)
-                except:
-                    add_message(traceback.format_exc())
+        Args:
+            queuelist:
+                A list of dictionaries where each contains a host UUID,
+                procedure UUID, and optionally a controller UUID.
 
-            return queuelist
-        except:
-            add_message(traceback.format_exc())
+        Returns:
+            A list of dictionaries where each contains a host UUID,
+            procedure UUID, and optionally a controller UUID.
+        """
+        for item in queuelist:
+            if "ctruuid" in item:
+                ctruuid = item["ctruuid"]
+            else:
+                ctruuid = None
+            queue_procedure(item["hstuuid"], item["prcuuid"], ctruuid)
+        return queuelist
 
+    @classmethod
     @cherrypy.expose
-    def get_queue_grid(self):
-        try:
-            return json.dumps(get_jobs_grid())
-        except:
-            add_message(traceback.format_exc())
+    @cherrypy.tools.json_out()
+    def get_queue_grid(cls) -> List[Dict]:
+        """This function registers the endpoint that returns the queued
+        and running procedures.
+
+        Returns:
+            A list of queue items.
+        """
+        return get_jobs_grid()
