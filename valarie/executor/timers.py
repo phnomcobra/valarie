670619594167b441f167eebc44240a1a9e324312
Justin Dierking <phnomcobra@gmail.com>
2022-07-01 10:57:59 -0400

    timers executor.

--- valarie/executor/timers.py
+++ valarie/executor/timers.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
+"""This module encapsulates the timers dictionary and implements
+timer cancellation."""
 
 from valarie.router.messaging import add_message
 
 timers = {}
 
 def cancel_timers():
-    for key in timers.keys():
+    """This function enumerates the timers and attempts to cancel
+    each timer."""
+    for key in timers.keys(): # pylint: disable=consider-iterating-dictionary
         try:
             timers[key].cancel()
             add_message(f"CANCELLING {key}...OK")
-        except Exception as e:
-            msg = str(e)
+        except Exception as exception:
+            msg = str(exception)
             add_message(f"CANCELLING {key}...{msg}")
